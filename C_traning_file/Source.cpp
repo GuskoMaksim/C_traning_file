e89c79d87f9d7b6857e9515da7b0603f9f029501
GuskoMaksim <gusko-maksim@mail.ru>
2020-10-20 21:36:09 +0300

    Добавьте файлы проекта.

---
+++ C_traning_file/Source.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include "Struct.h"
+
+using namespace std;
+
+void menu()
+{
+	system("cls");
+	cout << "1-Загрузить данные из файла " << endl;
+	cout << "2-Ввести данные " << endl;
+	cout << "3-Добавление данных " << endl;
+	cout << "4-Сортировка базы данных по алфавиту " << endl;
+	cout << "5-Сортировка базы данных по возрасту " << endl;
+	cout << "6-Вывод: список людей, родившихся в заданном месяце " << endl;
+	cout << "7-Вывод: фамилию самого старшего мужчины  " << endl;
+	cout << "8-Вывод: все фамилии, начинающиеся с заданной буквы " << endl;
+	cout << "0- Выход из программы " << endl;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Person f;
+	int vib = -1;
+	while (vib != 0)
+	{
+		menu();
+		//while (getchar() != '\n');
+		vib = _getch() - 48;
+		switch (vib)
+		{
+		case 1:
+		{
+			cout << "Введите имя файла ";
+			string name_file;
+			cin >> name_file;
+			f.read_file(name_file);
+			break;
+		}
+		case 2:
+		{
+			f.add_person();
+			break;
+		}
+		case 3:
+		{
+			string name_file;
+			cout << "Введите имя файла ";
+			cin >> name_file;
+			f.add_file(name_file);
+			break;
+		}
+		case 4:
+		{
+			f.sort_arr(4);
+			break;
+		}
+		case 5:
+		{
+			f.sort_arr(5);
+			break;
+		}
+		case 6:
+		{
+			int month;
+			cout << "Введите месяц ";
+			do
+			{
+				cin >> month;
+			} while ((month<0)&&(month>12));
+			f.month_chosen(month);
+			break;
+		}
+		case 7:
+		{
+			f.old_men_second_name();
+			break;
+		}
+		case 8:
+		{
+			char leter;
+			cin >> leter;
+			f.second_name_chosen_leter(leter);
+			break;
+		}
+		case 9:
+		{
+			f.show_person();
+			_getch();
+			break;
+		}
+		case 0:
+		{
+			vib = 0;
+			break;
+		}
+		}
+	}
+}
---
+++ C_traning_file/Struct.h
@@ -0,0 +1,228 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <conio.h>
+#include "Struct.h"
+
+using namespace std;
+
+struct Person_arr
+{
+	string FIO="";
+	int day_b = 0;
+	int month_b = 0;
+	int year_b = 0;
+	int gender = 0;
+	int old;
+};
+
+class Person
+{
+private:
+	string name_file;
+	int number_lines; 
+	ifstream file;
+	vector<Person_arr> arr;
+	void add(char new_num, int* ism)
+	{
+		int num = (int)new_num - 48;
+		*ism = (*ism) * 10 + (int)num;
+	}
+public:
+	Person() :
+		number_lines(0)
+	{}
+	void read_file(string new_name_file)
+	{
+		name_file = new_name_file;
+		file.open(name_file, ios_base::in);
+		char* str = new char[1024];
+		while (!file.eof())
+		{
+			Person_arr new_p;
+			int number_commas = 0;
+			file.getline(str, 1024, '\n');
+			for (int i = 0; str[i] != NULL; i++)
+			{
+				switch (number_commas)
+				{
+				case 1:
+					if (str[i] == ',')
+						number_commas++;
+					else
+						add(str[i], &new_p.day_b);
+					break;
+				case 2:
+					if (str[i] == ',')
+						number_commas++;
+					else
+						add(str[i], &new_p.month_b);
+					break;
+				case 3:
+					if (str[i] == ',')
+						number_commas++;
+					else
+						add(str[i], &new_p.year_b);
+					break;
+				case 4:
+					if (str[i] == ',')
+						number_commas++;
+					else
+						add(str[i], &new_p.gender);
+					break;
+				default:
+					if (str[i] != ',')
+						new_p.FIO += str[i];
+					else
+						number_commas++;
+					break;
+				}
+			}
+			new_p.old = 2020 - new_p.year_b;
+			number_lines++;
+			arr.push_back(new_p);
+		}
+		file.close();
+		delete[] str;
+		cout << "Продолжить";
+		_getch();
+	}
+	void add_person()
+	{
+		Person_arr new_p;
+		cout << "Введите ФИО" << endl;
+		cin >> new_p.FIO;
+		while (getchar() != '\n');
+		cout << "Введите дату рождения" << endl;
+		cin >> new_p.day_b;
+		while (getchar() != '\n');
+		cout << "Введите месяц рождения" << endl;
+		cin >> new_p.month_b;
+		while (getchar() != '\n');
+		cout << "Введите год рождения" << endl;
+		cin >> new_p.year_b;
+		cout << "Введите гендер (1-мужчина, 0-женщина)" << endl;
+		do
+		{
+			while (getchar() != '\n');
+			new_p.gender = _getch()-48;
+		}
+		while ((new_p.gender != 1) && (new_p.gender != 0));
+		new_p.old = 2020 - new_p.year_b;
+		number_lines++;
+		arr.push_back(new_p);
+		cout << "Продолжить";
+		_getch();
+	}
+	void add_file(string name_file)
+	{
+		ofstream o_file;
+		o_file.open(name_file, ios_base::out);
+		for (int i = 0; i < number_lines; i++)
+		{
+			o_file << arr[i].FIO << ',';
+			o_file << arr[i].day_b << ',';
+			o_file << arr[i].month_b << ',';
+			o_file << arr[i].year_b << ',';
+			o_file << arr[i].gender << endl;
+		}
+		file.close();
+		cout << "Продолжить";
+		_getch();
+	}
+	void show_person(int index = 0)
+	{
+		if (index == 0)
+			for (int i = 0; i < number_lines; i++)
+			{
+				cout << "ФИО:\t" << arr[i].FIO << endl;
+				cout << "\tДата рождения:" << endl;
+				cout << "\t" << arr[i].day_b << "." << arr[i].month_b << "." << arr[i].year_b << endl;
+			}
+		else
+		{
+			cout << "ФИО:\t" << arr[index].FIO << endl;
+			cout << "\tДата рождения:" << endl;
+			cout << "\t" << arr[index].day_b << "." << arr[index].month_b << "." << arr[index].year_b << endl;
+		}
+		cout << "Продолжить";
+		_getch();
+	}
+	void sort_arr(int vib)
+	{
+		switch (vib)
+		{
+		case 4:
+		{
+			auto comp = [](const Person_arr& f, const Person_arr& s)->bool
+			{
+				return f.FIO[0] < s.FIO[0];
+			};
+			sort(arr.begin(), arr.end(), comp);
+			break;
+		}
+		case 5:
+		{
+			auto comp = [](const Person_arr& f, const Person_arr& s)->bool
+			{
+				return f.old < s.old;
+			};
+			sort(arr.begin(), arr.end(), comp);
+			break;
+		}
+		}
+		cout << "Продолжить";
+		_getch();
+	}
+	void second_name_chosen_leter(char leter)
+	{
+		for (int i = 0; i < number_lines; i++)
+		{
+			if (arr[i].FIO[0] == leter)
+				cout << arr[i].FIO << endl;
+		}
+		cout << "Продолжить";
+		_getch();
+	}
+	void month_chosen(int month)
+	{
+		cout << "Люди родившиеся в " << month << " месяце" << endl;;
+		for (int i = 0; i < number_lines; i++)
+		{
+			if (arr[i].month_b == month)
+				cout << "\t" << arr[i].FIO << endl;
+		}
+		cout << "Продолжить";
+		_getch();
+	}
+	void old_men_second_name()
+	{
+		int max = 0;
+		int index = -1;
+		for (int i = 0; i < number_lines; i++)
+		{
+			if ((arr[i].gender) && (arr[i].old > max))
+			{
+				max = arr[i].old;
+				index = i;
+			}
+		}
+		if (index!=-1)
+		{
+			cout << "Старший мужчина\n\t" << arr[index].FIO << endl;
+		}
+		cout << "Продолжить";
+		_getch();
+	}
+	~Person()
+	{
+		add_file("file_save.txt");
+	}
+};
+
+#endif // !STRUCT_H
